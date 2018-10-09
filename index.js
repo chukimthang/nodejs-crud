445f9906493bcb50f9f3a909902fd09195e1077e
chu kim thang <chukimthang94@gmail.com>
2018-10-09 15:11:30 +0700

    Setup pages

--- index.js
+++ index.js
@@ -16,6 +16,27 @@ app.use("/js", express.static(__dirname + "/node_modules/tether/dist/js"));
 app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
-app.listen(8080, "localhost", function(req, res){
-	console.log("Listening 8080");
+var connection = mysql.createConnection({
+	host : "localhost",
+	user : "root",
+	password : "",
+	database : "node_tutorial_db"
+});
+
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM events", function(err, result)
+	{
+    res.render("pages/index", {
+      siteTitle: siteTitle,
+      pageTitle: "Event Listing",
+      items: result
+    });
+  });
+});
+
+const siteTitle = "NodeJs Example";
+const baseURL = "http://localhost:3000"
+
+app.listen(3000, "localhost", function(req, res){
+	console.log("Listening 3000");
 });
